bf40846b6500f14c7900ab6de4b6e2df884d698f
btzr-io <btzr.io@gmail.com>
2018-03-09 14:49:16 -0700

    fix module export

--- src/renderer/utils/index.js
+++ src/renderer/utils/index.js
@@ -1,4 +1,4 @@
-function updateObject(oldObject, newValues) {
+export function updateObject(oldObject, newValues) {
   // Encapsulate the idea of passing a new object as the first parameter
   // to Object.assign to ensure we correctly copy data instead of mutating
   return Object.assign({}, oldObject, newValues)
