e3dec00af0ad9685721d175726132a033d715d2a
kodxana <romanrex19@int.pl>
2018-09-24 07:55:46 +0200

    Added Madiator2011 Song :)

--- src/renderer/utils/api.js
+++ src/renderer/utils/api.js
@@ -7,6 +7,7 @@ const claims = [
   'lbry://piano#f13c81bf0bc0bfbb608ac59d53e3112377e92919',
   'lbry://freemusic#d6bc54d741f11f41d71d34dbb573ca1b3e1ce6b9',
   'lbry://downloadmusic#4d108034e1945d2712bda6627d1d24519b49d4c0',
+  '@Madiator2011-Gaming#2b89981a12926260d15cd11af264cf13252b9721/DeskaklizmMiraculumParodiaDespacito',
 ]
 
 export default claims
