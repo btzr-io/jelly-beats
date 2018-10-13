540f52d2b49ed76692c549724442e457812a9e4b
btzr-io <btzr.io@gmail.com>
2018-10-13 12:31:02 -0600

    tidy up cache logic

--- src/renderer/unistore/actions/cache.js
+++ src/renderer/unistore/actions/cache.js
@@ -2,8 +2,6 @@ export default {
   storeTrack(state, uri, { fee, title, artist, outpoint, thumbnail }) {
     // Previous data from cache
     const prevTrack = state.cache[uri] || {}
-    // Status
-    const { completed, isPlaying, isAvailable, isDownloading } = prevTrack
     // New track data
     const track = {
       fee,
@@ -12,41 +10,10 @@ export default {
       artist,
       outpoint,
       thumbnail,
-      // Don't overwrite
-      completed,
-      isPlaying,
-      isAvailable,
-      isDownloading,
     }
     // Update cache
     return {
       cache: { ...state.cache, [uri]: { ...track } },
-    }
-  },
-
-  updateTrackStatus(state, uri, { completed, isPlaying, isAvailable, isDownloading }) {
-    const prevTrack = state.cache[uri]
-
-    // Track doesn't exists
-    if (!uri || !prevTrack) return state
-
-    const status = {
-      completed,
-      isPlaying,
-      isAvailable,
-      isDownloading,
-    }
-
-    // Update cache
-    return {
-      cache: {
-        ...state.cache,
-        // Select from cache
-        [uri]: {
-          ...state.cache[uri],
-          ...status,
-        },
-      },
     }
   },
 }
