883458566cf350cf2ef6760e8de83351dbe4b260
Autumn Smith <autumnsmith@Autumns-MacBook-Pro-2.local>
2018-10-15 22:21:07 -0700

    Changed render to call getNavLink to render navLinks, removed unused import and props

--- src/renderer/components/sidebar/view.jsx
+++ src/renderer/components/sidebar/view.jsx
@@ -1,56 +1,67 @@
 import React from 'react'
 import Icon from '@mdi/react'
 import classnames from 'classnames'
-import navigate from '@/utils/navigate'
-import Button from '@/components/common/button'
-import * as icons from '@/constants/icons'
+import {
+  HOME as home,
+  PLAYLIST_PLUS as playlistPlus,
+  HEART_OUTLINE as heart,
+} from '@/constants/icons'
 
-const NavLink = ({ icon, label, active, action, badge }) => {
-  return (
-    <div
-      onClick={action}
-      className={classnames('nav__link', { 'nav__link--active': active })}
-    >
-      <div className="link__body">
-        {icon && <Icon className="icon link__icon" path={icon} />}
-        {label && <span className="link__label">{label}</span>}
-      </div>
-      <div className="link__actions">
-        {badge && <div className="link__badge">{badge}</div>}
-      </div>
+const NavLink = ({ icon, label, active, action, badge }) => (
+  <div
+    onClick={action}
+    className={classnames('nav__link', { 'nav__link--active': active })}
+  >
+    <div className="link__body">
+      {icon && <Icon className="icon link__icon" path={icon} />}
+      {label && <span className="link__label">{label}</span>}
     </div>
-  )
-}
+    <div className="link__actions">
+      {badge && <div className="link__badge">{badge}</div>}
+    </div>
+  </div>
+)
 
 class SideBar extends React.PureComponent {
   constructor(props) {
     super(props)
   }
 
-  render() {
-    const { doNavigate, navigation, favorites } = this.props
+  getNavLink = ({ path, label, icon }) => {
+    const { doNavigate, navigation } = this.props
     const { currentPage } = navigation || {}
+    return (
+      <NavLink
+        action={() => doNavigate(path)}
+        label={label}
+        icon={icon}
+        active={currentPage == path}
+      />
+    )
+  }
+
+  render() {
+    const navLinks = [
+      {
+        label: 'Home',
+        path: '/',
+        icon: home,
+      },
+      {
+        label: 'Playlists',
+        path: '/playlists',
+        icon: playlistPlus,
+      },
+      {
+        label: 'Favorites',
+        path: '/favorites',
+        icon: heart,
+      },
+    ]
     return (
       <div className="sidebar">
         <div className="nav__links">
-          <NavLink
-            action={() => doNavigate('/')}
-            label="Home"
-            icon={icons.HOME}
-            active={currentPage == '/'}
-          />
-          <NavLink
-            label="Playlists"
-            action={() => doNavigate('/playlists')}
-            icon={icons.PLAYLIST_PLUS}
-            active={currentPage == '/playlists'}
-          />
-          <NavLink
-            action={() => doNavigate('/favorites')}
-            label="Favorites"
-            icon={icons.HEART_OUTLINE}
-            active={currentPage == '/favorites'}
-          />
+          {navLinks.map((item, idx) => this.getNavLink({ ...item }))}
         </div>
       </div>
     )
