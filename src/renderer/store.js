63f4eb2953d1101e92533b2077ccffd5b9b09531
btzr-io <btzr.io@gmail.com>
2019-01-31 17:50:55 -0700

    minor patch for release build

--- src/renderer/store.js
+++ src/renderer/store.js
@@ -1,9 +1,11 @@
 import createStore from 'stockroom'
 import StoreWorker from '@/store.worker'
 
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
 const store = createStore(new StoreWorker())
 
-// Simple debug
-store.subscribe(console.log)
+// Debug store events
+isDevelopment && store.subscribe(console.log)
 
 export default store
